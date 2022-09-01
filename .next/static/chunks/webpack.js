e0a7134ebef5b52d7be6dd55b18a9065e3ffe0b3
Quintin Vomocil <quintinvomocil@gmail.com>
2022-09-01 15:01:29 -0600

    Restructed components

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3db4762fd9efa6a6"; }
+/******/ 		__webpack_require__.h = function() { return "7e67c7f9bea085c8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
-const IndexPage = () => {
-    return(
-    <div>Hello From Planet Earth!</div>
-    );
-}
+import { data } from "../SpeakerData";
+import Speaker from "../src/components/Speaker"
 
+const IndexPage = () => {
+  return (
+    <div className="container speakers-list">
+      <div className="row">
+        {data.map(function (speaker) {
+          return (
+             <Speaker key={speaker.id} speaker={speaker} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default IndexPage;
